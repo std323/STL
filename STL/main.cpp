c7c221129bd0c8811d150379ed45409bc3e8837f
Галима <@DESKTOP-PGIEF0N>
2023-06-15 20:37:02 +0300

    STL  DONE

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,5 +1,8 @@
-#include<iostream>
+ï»¿#include<iostream>
 #include<array>
+#include<stack>
+#include<vector>
+#include<deque>
 
 using std::cout;
 using std::cin;
@@ -8,7 +11,12 @@ using std::endl;
 #define tab "\t"
 #define delimiter   "\n----------------------------------\n"
 
-#define STL_ARRAY
+template<typename T>void vector_properties(const std::vector<T>& vec);
+
+//#define STL_ARRAY
+//#define STL_STACK
+//#define STL_VECTOR
+#define STL_QUEE
 
 void main()
 {
@@ -16,14 +24,85 @@ void main()
 	//cout << "Hello STL" << endl;
 
 #ifdef STL_ARRAY
-	//array - ýòî êîíòåéíåð, êîòîðûé õðàíèò äàííûå â âèäå ñòàòè÷åñêîãî ìàññèâà
+	//array - ÑÑ‚Ð¾ ÐºÐ¾Ð½Ñ‚ÐµÐ¹Ð½ÐµÑ€, ÐºÐ¾Ñ‚Ð¾Ñ€Ñ‹Ð¹ Ñ…Ñ€Ð°Ð½Ð¸Ñ‚ Ð´Ð°Ð½Ð½Ñ‹Ðµ Ð² Ð²Ð¸Ð´Ðµ ÑÑ‚Ð°Ñ‚Ð¸Ñ‡ÐµÑÐºÐ¾Ð³Ð¾ Ð¼Ð°ÑÑÐ¸Ð²Ð°
 	const int N = 5;
-	std::array<int, N> arr = {3,5,8};
+	std::array<int, N> arr = { 3,5,8 };
 	for (int i = 0; i < arr.size(); i++)
 	{
 		cout << arr[i] << tab;
 	}
 	cout << endl;
 #endif STL_ARRAY
+#ifdef STL_STACK
+	std::stack<int> stack;
+	stack.push(3);
+	stack.push(5);
+	stack.push(8);
+	stack.push(13);
+	stack.push(21);
+	while (!stack.empty())
+	{
+		cout << stack.top() << endl;
+		stack.pop();
+	}
+#endif // STL_STACK
+
+#ifdef STL_VECTOR
+	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	cout << endl;
+	vector_properties(vec);
+	vec.push_back(55);
+	vector_properties(vec);
+	vec.reserve(25);
+	//vec.shrink_to_fit();
+	vec.resize(8);
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	vector_properties(vec);
+
+	cout << delimiter << endl;
+	cout << std::vector<int>().max_size() << endl;
+	cout << std::vector<double>().max_size() << endl;
+
+	/*template<typename T>void vector_properties(const std::vector<T>& vec)
+{
+	cout << "Size: " << vec.size() << endl;
+	cout << "Max size:" << vec.max_size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+	cout << delimiter << endl;
+}*/
+
+#endif // STL_VECTOR	
+#ifdef STL_QUEE
+	std::deque<int> deque = { 3, 5, 8, 13, 21, 34 };
+	for (int i = 0; i < deque.size(); i++)
+	{
+		cout << deque[i] << tab;
+	}
+	cout << endl;
+
+	deque.push_back(55);
+	deque.push_back(45);
+	deque.push_back(38);
+
+	deque.pop_front();
+	deque.pop_front();
+	
+	for (std::deque<int>::iterator it = deque.begin(); it != deque.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+
+#endif // STL_QUEE
+
 
 }
+
